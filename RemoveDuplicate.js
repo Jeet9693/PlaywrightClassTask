a36c15caed3bf2ea9367900af1519099b5bf67a0
Jeet9693 <jeet9693@gmail.com>
2026-03-11 20:53:49 +0530

    RemoveDuplicate

---
+++ RemoveDuplicate.js
@@ -0,0 +1,8 @@
+let numbers = [12, 12, 14, 13, 11, 21, 21, 22, 22];
+
+let withoutDuplicate = [...new Set(numbers)].sort((a,b)=>a-b);
+
+
+console.log("Actual Numbers :- "+numbers);
+
+console.log("Numbers without duplicates are :-"+withoutDuplicate);
